9b1cbafe873dcb98a3adfa595e8a5fdc6d457406
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-02-10 05:43:13 -0800

    Add files via upload

---
+++ line.py
@@ -0,0 +1,19 @@
+import sys
+import os
+class line:
+    def __init__(self):
+        self.self = self
+        i = sys.dllhandle
+        n = sys.gettrace()
+        print(i, n)
+    def __init_subclass__(self):
+        o = os._exists(self)
+        u = os.cpu_count()
+        t = os.urandom(int(sys.argv[1]))
+        print(o, u, t)
+    def __getattribute__(self):
+        print(os.__getattribute__(self))
+class dip(line):
+    def __init__(self):
+        super(dip, self).__init__()
+line()
